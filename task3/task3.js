01e56eff2faeb378a7fcb62968b938a73f8d531c
Awaliyatul Hikmah <57889114+ahikmah@users.noreply.github.com>
2021-03-03 20:49:14 +0700

    task3 revisi

--- task3/task3.js
+++ task3/task3.js
@@ -1,19 +1,21 @@
-"use strict";
+'use strict';
 
 const divideAndSort = function(num) {
-    if (num && typeof num !== "number") {
-        return "Data harus bertipe number dan lebih dari 0";
+    if (num && typeof num !== 'number') {
+        return 'Data harus bertipe number dan lebih dari 0';
     } else {
         const result = [];
+
         const divArr = num.toString().split(0); // pisahkan berdasarkan 0
         // console.log(divArr);
 
-        divArr.forEach((val) => {
-            // console.log(val.split("").sort());
-            result.push(val.split("").sort().join(""));
+        divArr.forEach(val => {
+            const sorting = val.split('').sort();
+            result.push(...sorting);
         });
         // console.log(result);
-        return result.join("");
+
+        return result.join('');
     }
 };
 
