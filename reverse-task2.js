f0dd8d93e729fbde0f7b0742dc74f34c210fe34b
Awaliyatul Hikmah <57889114+ahikmah@users.noreply.github.com>
2021-03-04 10:13:38 +0700

    add validation

--- reverse-task2.js
+++ reverse-task2.js
@@ -1,21 +1,27 @@
-const data = 'Saya belajar Javascript';
-
-const words = [];
-let count = 0;
-words[count] = '';
-
-for (let i = 0; i < data.length; i++) {
-    if (data[i] !== ' ') {
-        words[count] += data[i];
+const reverseWord = function(data) {
+    if (typeof data !== 'string') {
+        console.log('Data harus bertipe string');
     } else {
-        count++;
+        const words = [];
+        let count = 0;
         words[count] = '';
-    }
-}
 
-// reverse word
-let reverse = '';
-for (let i = words.length - 1; i >= 0; i--) {
-    reverse += words[i] + ' ';
-}
-console.log(reverse);
+        for (let i = 0; i < data.length; i++) {
+            if (data[i] !== ' ') {
+                words[count] += data[i];
+            } else {
+                count++;
+                words[count] = '';
+            }
+        }
+
+        // reverse word
+        let reverse = '';
+        for (let i = words.length - 1; i >= 0; i--) {
+            reverse += words[i] + ' ';
+        }
+        console.log(reverse);
+    }
+};
+
+reverseWord('Saya belajar Javascript');
